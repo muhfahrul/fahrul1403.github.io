3558b3f65e401c38918ca0bbb90229c40053e2cb
fahrul1403 <103086852+fahrul1403@users.noreply.github.com>
2022-04-22 06:51:32 +0700

    Add files via upload

---
+++ buysell/pages/index.jsx
@@ -0,0 +1,15 @@
+import Header from "../src/components/Header/Header";
+import LandingPage from "../src/components/LandingPage/LandingPage";
+import FooterComponent from "../src/components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <LandingPage />
+      <FooterComponent />
+    </>
+  );
+};
+
+export default Home;
