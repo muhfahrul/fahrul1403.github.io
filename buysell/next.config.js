276fef823b70b675182ebe257e108b6f3ebdb748
fahrul1403 <103086852+fahrul1403@users.noreply.github.com>
2022-04-24 02:50:41 +0700

    Delete buysell directory

--- buysell/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  strictMode: true,
-}
-
-module.exports = nextConfig
--- buysell/src/helpers/string.js
+++
@@ -1,7 +0,0 @@
-const checkSingleDateValue = (value) => {
-  return value.toString().length === 1
-    ? `0${value.toString()}`
-    : value.toString();
-};
-
-export { checkSingleDateValue };
